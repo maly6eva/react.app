5de6dc09bcda6b95ebbfc56bca28963daae93abc
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-16 23:07:02 +0300

    кнопка

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Button from './components/Button/Button.jsx';
+
+function App() {
+
+  return (
+      <>
+        <h1>Зоголовок</h1>
+        <p>Какой-то текст</p>
+          <Button/>
+      </>
+  );
+}
+
+export default App;
