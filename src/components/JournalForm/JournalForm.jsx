5e0ca52abbafba8d6b7c33326087a43cff5cbc37
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-07-21 13:35:03 +0300

    forwardRef

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,13 +1,12 @@
 import styles from'./JournalForm.module.css';
-import Button from '../Button/Button.jsx';
-import {useEffect, useReducer} from 'react';
+import Button from '../Button/Button';
+import {useEffect, useReducer, useRef} from 'react';
 import cn from 'classnames';
-import { INITIAL_STATE, formReducer } from './JournalForm.state.js';
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
+import Input from '../Input/Input';
+// import { UserContext } from '../UserContext';
 
-
-
-
-function JournalForm({ onSubmit }) {
+function JournalForm({ onSubmit}) {
 	const [formState, dispatchForm] = useReducer(formReducer, INITIAL_STATE);
 	const { isValid, isFormReadyToSubmit, values } = formState;
 	const titleRef = useRef();
@@ -60,18 +59,14 @@ function JournalForm({ onSubmit }) {
 	return (
 		<form className={styles ['journal-form']} onSubmit={addJornalItem}>
 			<div>
-				<input type='text' ref={titleRef} onChange={onChange} value={values.title} name='title' className={cn(styles['input-title'], {
-					[styles['invalid']]: !isValid.title
-				})}/>
+				<Input type='text' ref={titleRef}  onChange={onChange} value={values.title} name='title' inValid={!isValid.title}/>
 			</div>
 			<div className={styles['form-row']}>
 				<label htmlFor="date" className={styles['form-label']}>
 					<img src="/data.svg" alt="Иконка календаря"/>
 					<span>Дата</span>
 				</label>
-				<input type='date' ref={dateRef} onChange={onChange} name='date' value={values.data}  id='date' className={cn(styles['input'], {
-					[styles['invalid']] : !isValid.date
-				})}/>)
+				<Input type='date' ref={dateRef} onChange={onChange} name='date' value={values.data}  id='date' inValid={!isValid.date} />
 			</div>
 
 			<div className={styles['form-row']}>
@@ -79,11 +74,12 @@ function JournalForm({ onSubmit }) {
 					<img src="/folder.svg" alt="Иконка папки"/>
 					<span>Метки</span>
 				</label>
-				<input type='text' onChange={onChange} id='tag' value={values.tag} name='teg' className={styles['input']}/>
+				<Input type='text' onChange={onChange} id='tag' value={values.tag} name='teg'/>
 			</div>
 
 
-			<textarea ref={postRef} name='post' id="" onChange={onChange} value={values.post}  cols="30" rows="10" className={cn(styles['input'], {[styles['invalid']]: !isValid.post})}></textarea>
+			<textarea ref={postRef} name='post' id="" onChange={onChange} value={values.post}  cols="30" rows="10" className={cn(styles['input'], {
+				[styles['invalid']]: !isValid.post})}></textarea>
 			<Button text="Сохранить"/>
 		</form>
 
